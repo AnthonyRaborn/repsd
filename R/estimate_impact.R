1cc798432e63ab021e7eb531fc38bb9183df3f9d
Anthony Raborn <araborn@nabp.pharmacy>
2022-11-18 11:42:11 -0500

    Added func arg checks to estimate_impact and updated @return

--- R/estimate_impact.R
+++ R/estimate_impact.R
@@ -7,7 +7,7 @@
 #'
 #' @return A `numeric` estimate of the impact as the effect size *D*, e.g., the
 #' standardized mean theta difference between the focal group and the composite
-#' (total) group abilities.
+#' (total) group abilities. This estimate is rounded to 3 decimal places.
 #'
 #' @importFrom stats var
 #' @export
@@ -17,7 +17,43 @@ estimate_impact <-
     responses = timmsData,
     focal_column = 21,
     focal_id = 1) {
-
+    ###############################################
+    ########## testing argument values ############
+    if (!(is.data.frame(responses) | is.matrix(responses))) {
+      stop('responses was not provided as a data.frame or a matrix.\nPlease provide the data in one of these classes.')
+    }
+    if (!(is.numeric(focal_column) | is.character(focal_column))) {
+      stop('focal_column needs to be a numeric or character value.')
+    }
+    focal_columnTest <- tryCatch(
+      expr = {
+        if (is.character(focal_column)) {
+          focal_column = which(colnames(responses) == focal_column)
+        }
+        responses[,focal_column]
+        responses[,-focal_column]
+        "Subsetting works."
+      },
+      error = function(e) e
+    )
+    if (any(class(focal_columnTest) == 'error')) {
+      stop('Problems with subsetting the focal column.\nPlease test that your responses data can be subsetting with responses[,focal_column] and responses[,-focal_column] (if focal_column is numeric) or that the correct column name was proved for focal_column (if focal_column is character).')
+    }
+    focalIDTest <- tryCatch({
+      if (is.character(focal_column)) {
+        focal_column = which(colnames(responses) == focal_column)
+      }
+      responses[responses[,focal_column] == focal_id, ]
+    },
+    error = function(e) e
+    )
+    if (any(class(focalIDTest) == 'error')) {
+      stop('Problems with the focal_id argument - error when attempting to subset the data.\nPlease check your provided focal_column and focal_id values and try again.')
+    }
+    if (nrow(focalIDTest) == 0) {
+      stop('Problems with identifying members of the focal group - no observations found.\nPlease check your provided focal_column and focal_id values and try again.')
+    }
+    ###############################################
     response_matrix = responses[, -focal_column]
 
     composite_ss = nrow(responses)
