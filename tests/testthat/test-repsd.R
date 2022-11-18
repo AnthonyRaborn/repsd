3a4ce53bdf1317625aaa305da225eeb490b03935
Anthony Raborn <araborn@nabp.pharmacy>
2022-11-18 11:43:38 -0500

    Simplified repsd tests

--- tests/testthat/test-repsd.R
+++ tests/testthat/test-repsd.R
@@ -2,19 +2,41 @@ test_that("repsd returns list of length 4", {
   expect_equal(4, length(repsd()))
 })
 
-test_that("repsd repsd_each_item defaults return 20 repsd with specified values", {
+test_that("repsd repsd_each_item defaults return 20 repsd values", {
   test_repsd_output =
     repsd()
 
   expect_equal(20, length(test_repsd_output$repsd_each_item))
-  expect_equal(canon_repsd_output$repsd_each_item, test_repsd_output$repsd_each_item)
 })
 
-test_that('repsd returns the correct number of removed observation', {
+test_that('repsd returns the correct values overall', {
   test_repsd_output =
     repsd()
 
-  expect_equal(canon_repsd_output$total_focal_removed, test_repsd_output$total_focal_removed)
-  expect_equal(canon_repsd_output$total_non_focal_removed, test_repsd_output$total_non_focal_removed)
-  expect_equal(canon_repsd_output$num_removed_over_foc_max, test_repsd_output$num_removed_over_foc_max)
+  expect_identical(canon_repsd_output, test_repsd_output)
 })
+
+test_that('repsd returns errors with improper arguments', {
+  expect_error(
+    repsd(responses = list(timmsData))
+  )
+  expect_error(
+    repsd(focalColumn = 22)
+  )
+  expect_error(
+    repsd(focalColumn = 'a')
+  )
+  expect_error(
+    repsd(focalGroupID = 13)
+  )
+  expect_error(
+    repsd(focalGroupID = 'a')
+  )
+  expect_error(
+    repsd(matching = 0)
+  )
+  expect_error(
+    repsd(matching = 'a')
+  )
+})
+
