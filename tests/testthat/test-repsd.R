20c8f58d8522111f7ffe5f17f17256b0a6e82dfd
Anthony Raborn <araborn@nabp.pharmacy>
2022-11-18 11:04:01 -0500

    Compare respd() output to canonical results

--- tests/testthat/test-repsd.R
+++ tests/testthat/test-repsd.R
@@ -7,4 +7,14 @@ test_that("repsd repsd_each_item defaults return 20 repsd with specified values"
     repsd()
 
   expect_equal(20, length(test_repsd_output$repsd_each_item))
+  expect_equal(canon_repsd_output$repsd_each_item, test_repsd_output$repsd_each_item)
+})
+
+test_that('repsd returns the correct number of removed observation', {
+  test_repsd_output =
+    repsd()
+
+  expect_equal(canon_repsd_output$total_focal_removed, test_repsd_output$total_focal_removed)
+  expect_equal(canon_repsd_output$total_non_focal_removed, test_repsd_output$total_non_focal_removed)
+  expect_equal(canon_repsd_output$num_removed_over_foc_max, test_repsd_output$num_removed_over_foc_max)
 })
