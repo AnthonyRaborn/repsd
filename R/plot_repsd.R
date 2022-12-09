a5b3f579aa7eb050ad79e5bd14ba2cf6a69a9838
Anthony Raborn <araborn@nabp.pharmacy>
2022-12-09 10:42:17 -0500

    Added roxygen manual

--- R/plot_repsd.R
+++ R/plot_repsd.R
@@ -1,3 +1,47 @@
+#' REPSD Null vs Observed Histogram
+#'
+#' @param repsd_values A numerical vector of repsd values, the output of `repsd()$repsd_each_item`.
+#' @param null_values A matrix of the repsd null distribution, the output of `null_repsd()`.
+#' @param pvalues A numerical vector of the repds p-values, the output of `repsd_pval()$p.value`
+#' @param which_item A numerical indicator of the specific item to plot.
+#' @param bins A numerical indicator on the number of bins to output in the histogram.
+#'
+#' @return A `plot` of the REPSD null distribution for the indicated item with
+#' the observed REPSD value as a red line and the observed p-value
+#' @export
+#'
+#' @examples
+#' # Not run
+#' \dontrun{
+#' example_repsd <-
+#'     repsd()
+#' example_null <-
+#'     null_repsd()
+#' example_pvals <-
+#'     repsd_pval(
+#'                alpha = .05,
+#'                null_dist = example_null,
+#'                items_repsd = example_repsd$repsd_each_item
+#'                )
+#' # Only one plot
+#' plot_repsd(repsd_values = example_repsd$repsd_each_item,
+#'            null_values = example_null,
+#'            pvalues = example_pvals$p.value,
+#'            which_item = 18,
+#'            bins = 50)
+#' # Multiple plots on the same plot
+#' par(mfrow=c(2,2))
+#' for (i in c(1,8,16,18)) {
+#'   plot_repsd(
+#'              repsd_values = example_repsd$repsd_each_item,
+#'              null_values = example_null,
+#'              pvalues = example_pvals$p.value,
+#'              which_item = 18,
+#'              bins = 50
+#'              )
+#'   }
+#' }
+
 plot_repsd <-
   function(repsd_values, null_values, pvalues, which_item, bins = 30) {
     hist(null_values[,which_item], breaks = bins,
@@ -10,13 +54,4 @@ plot_repsd <-
     mtext(paste0("For Item Number ", which_item))
   }
 
-plot_repsd(repsd_values, null_values, pvalues, 18, 50)
-
-par(mfrow=c(2,2))
-
-for (i in c(1,8,16,18)) {
-
-  plot_repsd(repsd_values, null_values, pvalues, i, 50)
-
-}
 
